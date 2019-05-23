4bc481b64875de76d152767d05bcf00f94ff1b3e
Fábio Vicentini <vct.fabio.vct@gmail.com>
2019-05-23 16:35:19 -0300

    criação de dados_ususario.php  e acrescimo da busca de produtos no sistema_interno.php

--- sistema_interno.php
+++ sistema_interno.php
@@ -1,43 +1,27 @@
 <?php
-
 include 'header.html';
-?>
-<!--<h1>Seja bem vindo</h1>-->
-
-
-<?php
 include 'Conection.php';
 
 session_start();
-echo $_SESSION['email'];
-$email = $_SESSION['email'];
 
-$query1 = "SELECT * FROM usuarios WHERE EMAIL = '$email'";
+include 'dados_usuarios.php';
 
-$consulta1 = mysqli_query($conexao, $query1);
+if(isset($_POST['codigo_produto'])){
+$codigo_produto = ($_POST['codigo_produto']);
+$query_buscar_produtos = "SELECT * FROM produtos WHERE Codigo = '$codigo_produto'";
+$consulta_buscar_produtos = mysqli_query($conexao, $query_buscar_produtos);
 
-while($linha = mysqli_fetch_array($consulta1)){
+while($produto = mysqli_fetch_array($consulta_buscar_produtos)){
 				
-    echo '<td>'.$linha['id_usuarios'].'</td><br>';
-    echo '<td>'.$linha['nome'].'</td><br>';
-    echo '<tr><td >'.$linha['sobrenome'].'</td><br>';
-    echo '<tr><td >'.$linha['telefone'].'</td><br>';
-    echo '<tr><td >'.$linha['CPF'].'</td><br>';
-}
-
+    echo '<td>'.$produto['Codigo'].'</td><br>';
+    echo '<td>'.$produto['Linha'].'</td><br>';
+    
+}}
 ?>
-
-
-
-
-<a href="logout.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Logout</a>
-
-
+  
 
 <?php
-
 include 'footer.html';
-
 ?>
 
 
